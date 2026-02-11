4ed53a30f30ba5e74af1f68b0ca4aff60e541fbf
Ayush Mishra <ayushlkw@gmail.com>
2026-02-11 17:00:57 +0530

    added bookmarker api in the project

--- src/main/java/com/K8s/bookmark/controller/BookmarkController.java
+++ src/main/java/com/K8s/bookmark/controller/BookmarkController.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/bookmarks")
+@RequestMapping("/api/bookmark")
 @RequiredArgsConstructor
 public class BookmarkController {
 
